e52e9b8a20393fe73a36f3dab0ab3f8964858dc7
Daniel Robaszkiewicz <daniel.robaszkiewicz@wp.pl>
2024-02-06 15:42:28 +0100

    fixes

---
+++ ModelingEvolution.DirectConnect.Tests/MessageRequestHandler.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+using ProtoBuf;
+
+namespace ModelingEvolution.DirectConnect.Tests;
+
+public class MessageRequestHandler : RequestHandlerBase<Message>
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly TypeRegister _typeRegister;
+
+    public MessageRequestHandler(IServiceProvider service)
+    {
+        _serviceProvider = service;
+        _typeRegister =  _serviceProvider.GetRequiredService<TypeRegister>();
+    }
+    public override async Task Handle(Message request)
+    {
+        var typeOfFooRequest = _typeRegister.GetRequiredType(request.TypeId);
+
+        Type type = typeof(IRequestHandler<>).MakeGenericType(typeOfFooRequest);
+
+        var handler = (IRequestHandler)_serviceProvider.GetRequiredService(type);
+        var deserializedObj = Serializer.NonGeneric.Deserialize(typeOfFooRequest, request.Data.AsSpan());
+        handler.Handle(deserializedObj);
+    }
+        
+}
--- ModelingEvolution.DirectConnect.Tests/UnitTest1.cs
+++ ModelingEvolution.DirectConnect.Tests/UnitTest1.cs
@@ -31,28 +31,6 @@ namespace ModelingEvolution.DirectConnect.Tests
             return Handle((TRequest)request);
         }
     }
-    public class MessageRequestHandler : RequestHandlerBase<Message>
-    {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly TypeRegister _typeRegister;
-
-        public MessageRequestHandler(IServiceProvider service)
-        {
-          _serviceProvider = service;
-          _typeRegister =  _serviceProvider.GetRequiredService<TypeRegister>();
-        }
-        public override async Task Handle(Message request)
-        {
-            var typeOfFooRequest = _typeRegister.GetRequiredType(request.TypeId);
-
-            Type type = typeof(IRequestHandler<>).MakeGenericType(typeOfFooRequest);
-
-            var handler = (IRequestHandler)_serviceProvider.GetRequiredService(type);
-            var deserializedObj = Serializer.NonGeneric.Deserialize(typeOfFooRequest, request.Data.AsSpan());
-            handler.Handle(deserializedObj);
-        }
-        
-    }
 
     public class RequestSink<TRequest>: IRequestHandler<Message>
     {
